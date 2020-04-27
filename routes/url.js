53938e7b92d6ec02ed9d4a17f9d3170e04a4acfb
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-04-28 02:05:41 +0800

    Attempt new url route syntax using .then() chain

--- routes/url.js
+++ routes/url.js
@@ -11,78 +11,140 @@ router.get('/', (req, res) => {
 })
 
 // 送出原始網址到 server 處理 + 記錄
-router.post('/', (req, res, next) => {
+router.post('/', (req, res) => {
   Url.findOne({ originalUrl: req.body.originalUrl })
     .lean()
-    .exec((err, url) => {
-      if (err) return console.log('error', err)
+    .then((err, url) => {
+      if (err) return console.log(err)
 
-      // 確認輸入的網址是否存在在網路上
-      let existence
+      if (url) {
+        console.log('網址已存在於 DB')
+        res.render('generated', {
+          originalUrl: req.body.originalUrl,
+          domain,
+          shortUrlKey: url.shortUrlKey
+        })
+      } else {
+        console.log('網址不存在於 DB')
+        return '確認新連結是否存在'
+      }
+    })
+    .then(message => {
+      console.log('階段任務：', message)
 
-      (async () => {
-        existence = await urlExist(req.body.originalUrl)
+      let result
+      const checkUrlExistence = async function () {
+        result = await urlExist(req.body.originalUrl)
 
-        console.log('existence', existence)
+        if (result) {
+          console.log('網址存在!')
 
-        if (existence) {
-          // URL 活在網路上
-          console.log('URL alive!')
+          let key = generateKey(5)
 
-          if (url) {                            // 如果 url 紀錄已存在於資料庫
-            console.log('URL found in the database', url)
+          const keyQuery = Url.findOne({ shortUrlKey: key })
+            .lean()
+            .exec((err, url) => {
+              if (url) {
+                console.log('DB 中發現重複的 key')
+                return keyQuery
+              } else {
+                console.log('這是獨一無二的 key')
 
-            res.locals.shortUrlKey = url.shortUrlKey
+                const newUrl = new Url({
+                  originalUrl: req.body.originalUrl,
+                  shortUrlKey: key
+                })
 
-            return res.render('generated', {
-              originalUrl: req.body.originalUrl,
-              domain,
-              shortUrlKey: res.locals.shortUrlKey
+                newUrl.save(err => {
+                  if (err) return console.log(err)
+                  return res.render('generated', {
+                    originalUrl: req.body.originalUrl,
+                    domain,
+                    shortUrlKey: key
+                  })
+                })
+              }
             })
-          } else {                              // 如果 url 紀錄不存在於資料庫
-            // // 檢查 key 是否和資料庫中的其他 key 重複
-            // let key = generateKey(5)
-            // let keyCheck = true
-            // while (keyCheck) {
-            //   Url.findOne({ shortUrlKey: key }, (err, url) => {
-            //     console.log('in the while loop')
-            //     if (err) return console.log(err)
-
-            //     if (url) {
-            //       console.log('Repeated key found!')
-            //       key = generateKey(5)
-            //     } else {
-            //       keyCheck = false
-            //     }
-            //   })
-            // }
-
-            const newUrlRecord = new Url({
-              originalUrl: req.body.originalUrl,
-              shortUrlKey: generateKey(5)
-            })
-
-            res.locals.shortUrlKey = newUrlRecord.shortUrlKey
-
-            newUrlRecord.save(err => {
-              if (err) return console.log(err)
-              return res.render('generated', {
-                originalUrl: req.body.originalUrl,
-                domain,
-                shortUrlKey: res.locals.shortUrlKey
-              })
-            })
-          }
         } else {
-          // URL 不存在在網路上
-          console.log('DEAD URL!')
-          return res.render('index', {
+          console.log('網址不存在')
+          res.render('index', {
             originalUrl: req.body.originalUrl,
             warning_msg: '輸入的網址不存在'
           })
         }
-      })()
+      }
+
     })
+    .catch(error => console.log(error))
+  // .exec((err, url) => {
+  //   if (err) return console.log('error', err)
+
+  //   // 確認輸入的網址是否存在在網路上
+  //   let existence
+
+  //   (async () => {
+  //     existence = await urlExist(req.body.originalUrl)
+
+  //     console.log('existence', existence)
+
+  //     if (existence) {
+  //       // URL 活在網路上
+  //       console.log('URL alive!')
+
+  //       if (url) {                            // 如果 url 紀錄已存在於資料庫
+  //         console.log('URL found in the database', url)
+
+  //         res.locals.shortUrlKey = url.shortUrlKey
+
+  //         return res.render('generated', {
+  //           originalUrl: req.body.originalUrl,
+  //           domain,
+  //           shortUrlKey: res.locals.shortUrlKey
+  //         })
+  //       } else {                              // 如果 url 紀錄不存在於資料庫
+  //         // // 檢查 key 是否和資料庫中的其他 key 重複
+  //         // let key = generateKey(5)
+  //         // let keyCheck = true
+  //         // while (keyCheck) {
+  //         //   Url.findOne({ shortUrlKey: key }, (err, url) => {
+  //         //     console.log('in the while loop')
+  //         //     if (err) return console.log(err)
+
+  //         //     if (url) {
+  //         //       console.log('Repeated key found!')
+  //         //       key = generateKey(5)
+  //         //     } else {
+  //         //       keyCheck = false
+  //         //     }
+  //         //   })
+  //         // }
+
+  //         const newUrlRecord = new Url({
+  //           originalUrl: req.body.originalUrl,
+  //           shortUrlKey: generateKey(5)
+  //         })
+
+  //         res.locals.shortUrlKey = newUrlRecord.shortUrlKey
+
+  //         newUrlRecord.save(err => {
+  //           if (err) return console.log(err)
+  //           return res.render('generated', {
+  //             originalUrl: req.body.originalUrl,
+  //             domain,
+  //             shortUrlKey: res.locals.shortUrlKey
+  //           })
+  //         })
+  //       }
+  //     } else {
+  //       // URL 不存在在網路上
+  //       console.log('DEAD URL!')
+  //       return res.render('index', {
+  //         originalUrl: req.body.originalUrl,
+  //         warning_msg: '輸入的網址不存在'
+  //       })
+  //     }
+  //   })()
+  // })
 })
 
 
